f48b7015aa2af369c4fc6eb55486d8b684c93719
Timóteo <timoteoapolinario@gmail.com>
2019-09-05 23:32:37 -0300

    App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Banner :link="$url+'y3zct'" :header="['id','title','image_url']" />
+    <Products :link="$url+'dbycd'" :header="['id','name','image_url']" />
+    <b-container ><b-row align-content-center>
+        <b-col sm="8"> <About /> </b-col>
+        <b-col sm="4"> <News /> </b-col>
+    </b-row></b-container>
+    <Content />
+    <Partners />
+    <Contact />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+//import Header from './components/Header'
+//import Banner from './components/Banner'
+//import Products from './components/Products'
+//import About from './components/About'
+//import News from './components/News'
+//import Partners from './components/Partners'
+
+import Vue from 'vue';
+
+//import BootstrapVue from 'bootstrap-vue'
+//Vue.use(BootstrapVue)
+
+import { LayoutPlugin } from 'bootstrap-vue'
+Vue.use(LayoutPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+//Vue.config.silent = true
+
+//Vue.prototype.$url = 'https://api.myjson.com/bins/'
+Vue.prototype.$url = ''
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    //Header,
+    //Banner,
+    //Products,
+    //About,
+    //News,
+    //Partners,
+    //Contact,
+    //Footer,
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat-Bold', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: .55rem;
 }
 </style>
